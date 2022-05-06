a65935e185e060de941b8882070e6b7d5c674680
Steve Blythe <blythesr@gmail.com>
2022-05-06 20:20:06 +0100

    Initialise Firebase App in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:bethel_smallholding/app/sign_in/sign_in_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp();
   runApp(BethelSmallholdingApp());
 }
 

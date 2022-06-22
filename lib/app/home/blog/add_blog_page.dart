b448fb4c9632ee7a8cc60085523560e5f69f8fa0
Steve Blythe <blythesr@gmail.com>
2022-06-22 14:47:52 +0100

    Added Loading State to Add Blog Post Page

--- lib/app/home/blog/add_blog_page.dart
+++ lib/app/home/blog/add_blog_page.dart
@@ -57,7 +57,7 @@ class _AddBlogPageState extends State<AddBlogPage> {
   AddBlogPostModel get model => widget.model;
 
   Future<void> _submitForm() async {
-    model.submit();
+    model.updateWith(submittedTapped: true, submitting: true);
 
     if (_validateAndSaveForm()) {
       final blogPost = BlogPost(
@@ -76,6 +76,8 @@ class _AddBlogPageState extends State<AddBlogPage> {
           title: "Operation Failed",
           exception: e,
         );
+      } finally {
+        model.updateWith(submitting: false);
       }
     }
   }
@@ -96,15 +98,18 @@ class _AddBlogPageState extends State<AddBlogPage> {
         title: const Text("Add Blog Post"),
         elevation: 2.0,
         actions: [
-          TextButton(
-            child: const Text(
-              "Save",
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.white,
+          Visibility(
+            child: TextButton(
+              child: const Text(
+                "Save",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
               ),
+              onPressed: _submitForm,
             ),
-            onPressed: _submitForm,
+            visible: model.submitEnabled,
           ),
         ],
       ),
@@ -114,16 +119,24 @@ class _AddBlogPageState extends State<AddBlogPage> {
   }
 
   Widget _buildContents() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Card(
+    return Column(
+      children: [
+        Visibility(
+          child: const LinearProgressIndicator(),
+          visible: model.submitting,
+        ),
+        SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: _buildForm(),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: _buildForm(),
+              ),
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 
@@ -143,6 +156,7 @@ class _AddBlogPageState extends State<AddBlogPage> {
         decoration: InputDecoration(
           labelText: "Blog Post Title",
           errorText: model.titleErrorText,
+          enabled: !model.submitting,
         ),
         focusNode: _titleFocusNode,
         validator: (_) => model.titleErrorText,
@@ -154,21 +168,32 @@ class _AddBlogPageState extends State<AddBlogPage> {
         decoration: InputDecoration(
           labelText: "Blog Post Content",
           errorText: model.contentErrorText,
+          enabled: !model.submitting,
         ),
         focusNode: _contentFocusNode,
         maxLines: null,
         keyboardType: TextInputType.multiline,
         validator: (_) => model.contentErrorText,
-        textInputAction: TextInputAction.done,
+        textInputAction: TextInputAction.newline,
         onChanged: model.updateContent,
+        onEditingComplete: _contentEditingComplete,
       ),
     ];
   }
 
   void _titleEditingComplete() {
-    final newFocus = model.titleValidator.isValid(model.title)
-        ? _contentFocusNode
-        : _titleFocusNode;
+    model.titleNextTapped();
+
+    var newFocus = _titleFocusNode;
+    if (model.titleValidator.isValid(model.title)) {
+      newFocus = _contentFocusNode;
+    } else {
+      newFocus = _titleFocusNode;
+    }
     FocusScope.of(context).requestFocus(newFocus);
+  }
+
+  void _contentEditingComplete() {
+    model.contentDoneTapped();
   }
 }
--- lib/app/home/blog/add_blog_post_model.dart
+++ lib/app/home/blog/add_blog_post_model.dart
@@ -4,36 +4,58 @@ import 'package:flutter/foundation.dart';
 class AddBlogPostModel with BlogPostValidators, ChangeNotifier {
   String title;
   String content;
-  bool submitted;
+
+  bool submittedTapped;
+  bool submitting;
+  bool titleNextPressed;
+  bool contentDonePressed;
 
   AddBlogPostModel({
     this.title = "",
     this.content = "",
-    this.submitted = false,
+    this.submittedTapped = false,
+    this.submitting = false,
+    this.titleNextPressed = false,
+    this.contentDonePressed = false,
   });
 
   String? get titleErrorText =>
-      !titleValidator.isValid(title) && submitted ? titleValidator.error : null;
-
-  String? get contentErrorText =>
-      !contentValidator.isValid(content) && submitted
-          ? contentValidator.error
+      !titleValidator.isValid(title) && (submittedTapped || titleNextPressed)
+          ? titleValidator.error
           : null;
+
+  String? get contentErrorText => !contentValidator.isValid(content) &&
+          (submittedTapped || contentDonePressed)
+      ? contentValidator.error
+      : null;
+
+  bool get submitEnabled =>
+      titleValidator.isValid(title) &&
+      contentValidator.isValid(content) &&
+      !submitting;
 
   void updateTitle(String title) => updateWith(title: title);
 
   void updateContent(String content) => updateWith(content: content);
 
-  void submit() => updateWith(submitted: true);
+  void titleNextTapped() => updateWith(titleNextPressed: true);
+
+  void contentDoneTapped() => updateWith(contentDonePressed: true);
 
   void updateWith({
     String? title,
     String? content,
-    bool? submitted,
+    bool? submittedTapped,
+    bool? submitting,
+    bool? titleNextPressed,
+    bool? contentDonePressed,
   }) {
     this.title = title ?? this.title;
     this.content = content ?? this.content;
-    this.submitted = submitted ?? this.submitted;
+    this.submittedTapped = submittedTapped ?? this.submittedTapped;
+    this.submitting = submitting ?? this.submitting;
+    this.titleNextPressed = titleNextPressed ?? this.titleNextPressed;
+    this.contentDonePressed = contentDonePressed ?? this.contentDonePressed;
     notifyListeners();
   }
 }
